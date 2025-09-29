bae4ed83fa1c9f9f50865cf261e18b302c2bd75a
vicejo1009 <vicejo1009@gmail.com>
2025-09-29 20:38:20 +0000

    🔧Chore(IconButton): Se añadio la característica de configurar color de fondo a los iconos de manera personalizada

--- components/ui/IconButton.tsx
+++ components/ui/IconButton.tsx
@@ -7,15 +7,17 @@ interface IconButtonProps {
     Presionar?: () => void; //Funcion que se ejecuta al presoinar el boton
     color?:string //Color del icono (opcional)
     tam?:number //Tamaño del icono (opcional)
+    bgc?:string //Color de fondo del boton (opcional)
 }
 
-const IconButton = ({Icon, Presionar, color, tam}: IconButtonProps) =>{
+const IconButton = ({Icon, Presionar, color, tam, bgc}: IconButtonProps) =>{
     return (
-      <Pressable onPress={Presionar} className="p-2 rounded-full bg-gray-300">
+      <Pressable onPress={Presionar} className="p-2 rounded-full">
         <Ionicons 
           name={Icon as any} 
           size={tam} 
           color={color}
+          backgroundColor={bgc}
         />
       </Pressable>
     )
